ad16882dee9bad8289dadfdcf4e1cbfddce6a7a3
Gineton Alencar II <gineton2@gmail.com>
2018-01-19 19:50:52 -0800

    Added controller assets and TODO controller anims

--- Assets/Scripts/HeadsetSpin.cs
+++ Assets/Scripts/HeadsetSpin.cs
@@ -7,10 +7,14 @@ public class HeadsetSpin : MonoBehaviour {
     public float spinRate;
 
     private GameObject[] headsets;
+    private GameObject[] threeDofControllers;
+    private GameObject[] sixDofControllers;
 	// Use this for initialization
 	void Start ()
     {
         headsets = GameObject.FindGameObjectsWithTag("Headset");
+        threeDofControllers = GameObject.FindGameObjectsWithTag("3DofController");
+        sixDofControllers = GameObject.FindGameObjectsWithTag("6DofController");
 	}
 	
 	// Update is called once per frame
@@ -19,6 +23,11 @@ public class HeadsetSpin : MonoBehaviour {
         foreach (GameObject headset in headsets)
         {
             headset.transform.Rotate(Vector3.up * spinRate * Time.deltaTime, Space.World);
+        }
+
+        // TODO Add animations for 3DOF and 6DOF controllers
+        foreach (GameObject controller in threeDofControllers) {
+            //controller.transform.Rotate(Vector)
         }
 	}
 }
